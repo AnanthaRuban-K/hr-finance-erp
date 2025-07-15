c1e71e9e8d8dbf2d38d1e6be91977fc1177e7c35
AnanthaRuban-K <website@sbrostech.com>
2025-07-15 22:41:15 +0530

    remove approval page

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/providers/theme-provider'
-import { ClerkProvider } from '@clerk/nextjs'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -25,7 +24,7 @@ export default function RootLayout({
          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
         </ThemeProvider>
-      
+       
       </body>
       
     </html>
